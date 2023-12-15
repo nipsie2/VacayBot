3a130c441546f93b7ae945d6067783364f961b29
johnharry980 <151979517+johnharry980@users.noreply.github.com>
2023-12-15 21:17:31 +0700

    app_v1.py

--- llama2test/app_v1.py
+++ llama2test/app_v1.py
@@ -5,6 +5,21 @@ import os
 # App title
 st.set_page_config(page_title="🦙💬 Llama 2 Chatbot")
 
+# Replicate Credentials
+with st.sidebar:
+    st.title('🦙💬 Llama 2 Chatbot')
+    st.write('This chatbot is created using the open-source Llama 2 LLM model from Meta.')
+    if 'REPLICATE_API_TOKEN' in st.secrets:
+        st.success('API key already provided!', icon='✅')
+        replicate_api = st.secrets['REPLICATE_API_TOKEN']
+    else:
+        replicate_api = st.text_input('Enter Replicate API token:', type='password')
+        if not (replicate_api.startswith('r8_') and len(replicate_api)==40):
+            st.warning('Please enter your credentials!', icon='⚠️')
+        else:
+            st.success('Proceed to entering your prompt message!', icon='👉')
+    os.environ['REPLICATE_API_TOKEN'] = replicate_api
+    
 # Store LLM generated responses
 if "messages" not in st.session_state.keys():
     st.session_state.messages = [{"role": "assistant", "content": "How may I assist you today?"}]
